ffd5922c215be2f45aba143e8d8bd14c67941795
lachoneus8 <2500010+lachoneus8@users.noreply.github.com>
2025-10-18 06:03:35 -0600

    Basics for ants patrolling
    3 frame move animation
    Random time spent in idle
    Picks a destination in a patrol range around the starting point
    Has hooks in place to have a sight range and a smell range (smell range much further out)
    All sprites are white, so we can color them via materials or just using the color setting.

--- Assets/Scripts/Gameplay/AllyUnit.cs
+++ Assets/Scripts/Gameplay/AllyUnit.cs
@@ -1,16 +1,105 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class AllyUnit : MonoBehaviour
+public class AllyUnit : Entity
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public enum EAllyState
     {
-        
+        Idle,
+        Patrol,
+        MoveTo,
+        Attack
     }
 
-    // Update is called once per frame
-    void Update()
+    public float smellRange;
+    public float sightRange;
+
+    public float idleTimeAvg;
+    public float idleTimeVar;
+
+    public float patrolDistance;
+    public float moveSpeed;
+
+    private Animator animator;
+
+    private List<Entity> smellRangeEntities;
+    private List<Entity> sightRangeEntities;
+
+    public Vector3 curPatrolCenter;
+    public Vector3 curMoveTarget;
+
+    private float curStateTime;
+
+    private EAllyState curState = EAllyState.Idle;
+
+    private void Start()
     {
-        
+        animator = GetComponent<Animator>();
+
+        curPatrolCenter = transform.position;
+
+        curStateTime = Random.Range(0, idleTimeAvg + idleTimeVar);
+    }
+
+    private void Update()
+    {
+        curStateTime -= Time.deltaTime;
+
+        switch (curState)
+        {
+            case EAllyState.Idle:
+                HandleIdleState();
+                break;
+            case EAllyState.Patrol:
+                HandlePatrolState();
+                break;
+            case EAllyState.MoveTo:
+                
+                break;
+            case EAllyState.Attack:
+                
+                break;
+        }
+    }
+
+    private void HandlePatrolState()
+    {
+        var dist = (transform.position - curMoveTarget).magnitude;
+        if (dist < .1f)
+        {
+            curState = EAllyState.Idle;
+            curStateTime = Random.Range(idleTimeAvg - idleTimeVar, idleTimeAvg + idleTimeVar);
+            SetWalk(false);
+        }
+        else
+        {
+            var dir = (curMoveTarget - transform.position).normalized;
+            
+            // Rotate to face movement direction (top-down view, rotate around Z-axis)
+            if (dir != Vector3.zero)
+            {
+                float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90f;
+                transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            }
+            
+            transform.position += dir * moveSpeed * Time.deltaTime;
+        }
+    }
+
+    private void HandleIdleState()
+    {
+        if (curStateTime <= 0)
+        {
+            curState = EAllyState.Patrol;
+            Vector2 randomPoint = Random.insideUnitCircle * patrolDistance;
+            curMoveTarget = curPatrolCenter + new Vector3(randomPoint.x, randomPoint.y, 0);
+            SetWalk(true);
+        }
+    }
+
+    private void SetWalk(bool isWalking)
+    {
+        animator.SetBool("Walk", isWalking);
     }
 }
