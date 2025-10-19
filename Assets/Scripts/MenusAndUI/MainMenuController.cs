6c73d72a4c850374f9c09b74abdf46c1e74189d0
wksnders <42388072+wksnders@users.noreply.github.com>
2025-10-19 12:53:51 -0600

    added sfx files and music to main menu

--- Assets/Scripts/MenusAndUI/MainMenuController.cs
+++ Assets/Scripts/MenusAndUI/MainMenuController.cs
@@ -6,8 +6,16 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+
+
+    [Header("SFX")]
+    public AudioSource backgroundAmbianceAudioSource;
+    public AudioClip MenuThemeSFX;
     void Start()
     {
+        backgroundAmbianceAudioSource.clip = MenuThemeSFX;
+        backgroundAmbianceAudioSource.loop = true;
+        backgroundAmbianceAudioSource.Play();
     }
 
     void Update()
@@ -17,17 +25,20 @@ public class MainMenuController : MonoBehaviour
 
     public void LoadStartGameScene()
     {
+        backgroundAmbianceAudioSource.Stop();
         SceneManager.LoadSceneAsync(1);
     }
     public void LoadMainGameScene()
     {
+        backgroundAmbianceAudioSource.Stop();
         SceneManager.LoadSceneAsync(2);
     }
     public void QuitGame()
     {
+        backgroundAmbianceAudioSource.Stop();
         // If running in the Unity editor, stop play mode
-        #if UNITY_EDITOR
-            EditorApplication.isPlaying = false;
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
         #else
             Application.Quit();
         #endif
